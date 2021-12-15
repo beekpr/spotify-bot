74e07fb69c874ab88fabafd295830334c47c0503
Patryk Jucha <patryk.jucha@beekeeper.io>
2021-12-15 16:19:59 +0100

    BERI-738: Fixed log4j

--- src/main/java/io/beekeeper/xbot/Main.java
+++ src/main/java/io/beekeeper/xbot/Main.java
@@ -7,14 +7,14 @@ import java.io.File;
 
 public class Main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         Configuration config = null;
         try {
             config = mapper.readValue(new File("config/configuration.yaml"), Configuration.class);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            return;
         }
 
         new SpotifyToYoutubeBot(config).start();
